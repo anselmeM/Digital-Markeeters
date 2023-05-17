d723ff959d61bcf49c077bf089fddd30bc0774b5
anselme <amotchojr@gmail.com>
2023-05-17 02:15:08 -0400

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
-
 AOS.init({
-	offset: 400, // offset (in px) from the original trigger point
-	duration: 1000,
+  offset: 400,
+  duration: 1000,
 });
-// AOS animations
 
 $(document).ready(function() {
+  // Toggle navigation menu
+  $('.nav__toggler').click(function() {
+    $(this).toggleClass('active');
+    $('.ham-menu').toggleClass('active');
+  });
 
-	$('.nav__toggler').click(function () {
-		$(this).toggleClass('active');
-		$('.ham-menu').toggleClass('active');
-	});
-	$('.plus-minus-box--facebook').click(function () {
-		$(this).toggleClass('active');
-		$('.accordian-box__para--facebbok').slideToggle( 'slow' );
-	});
-	$('.plus-minus-box--google').click(function () {
-		$(this).toggleClass('active');
-		$('.accordian-box__para--google').slideToggle( 'slow' );
-	});
-	$('.plus-minus-box--linkedin').click(function () {
-		$(this).toggleClass('active');
-		$('.accordian-box__para--linkedin').slideToggle( 'slow' );
-	});
-	$('.menu__link').click(function () {
-		$('.nav__toggler').removeClass('active');
-		$('.ham-menu').removeClass('active');
-	});
-	$('.body-wrapper').click(function () {
-		$('.nav__toggler').removeClass('active');
-		$('.ham-menu').removeClass('active');
-	});
+  // Toggle Facebook accordion box
+  $('.plus-minus-box--facebook').click(function() {
+    $(this).toggleClass('active');
+    $('.accordian-box__para--facebook').slideToggle('slow');
+  });
+
+  // Toggle Google accordion box
+  $('.plus-minus-box--google').click(function() {
+    $(this).toggleClass('active');
+    $('.accordian-box__para--google').slideToggle('slow');
+  });
+
+  // Toggle LinkedIn accordion box
+  $('.plus-minus-box--linkedin').click(function() {
+    $(this).toggleClass('active');
+    $('.accordian-box__para--linkedin').slideToggle('slow');
+  });
+
+  // Close navigation menu on link click
+  $('.menu__link').click(function() {
+    $('.nav__toggler').removeClass('active');
+    $('.ham-menu').removeClass('active');
+  });
+
+  // Close navigation menu on body wrapper click
+  $('.body-wrapper').click(function() {
+    $('.nav__toggler').removeClass('active');
+    $('.ham-menu').removeClass('active');
+  });
 });
+
 
 		
